6b6fcdaf52e1024f9ab9ad66af916b64acbaadd6
rimshaarshad <rimshaarshad5@gmail.com>
2019-09-05 16:36:47 +0500

    working on showing list with check box

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ import './style.css';
 $(document).ready(function () {
   var arr = [];
   var check = [];
+  var inputs = "";
+  var list = "";
+  var btns = $(".btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = $(".active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
   $('#save').on('click', function () {
     var add = $('input[id=list-input]').val();
     if (add.length == 0) {
@@ -12,30 +22,30 @@ $(document).ready(function () {
     }
     else {
       $('#error').html("");
-      var chk = $('<input />').attr({ type: 'checkbox', name: 'Iqra' });
-      var li = $('<li></li>');
-      arr.push(add);
-      $(li).append(chk).append(add);
-      $('ol[id=list-output]').append(li);
+      $.fn.additem(add);
       $('input[id=list-input]').val('');
     }
   })
   $('#selected').on('click', function () {
     $('#heading').html();
-    var inputs = $('#output input');
-    $.each(arr, function (index, value) {
+    inputs = $('#output input');
+    list = $('#list-output li');
+    $.each(arr, function (index) {
       if (inputs[index].checked == true) {
-        check.push(value);
+        console.log("here" + list[index]);
+        check.push(list[index]);
       }
     })
     $.fn.view(check, "Checked item(s) are:");
     check = [];
   });
   $('#unselected').on('click', function () {
-    var inputs = $('#output input');
-    $.each(arr, function (index, value) {
+    inputs = $('#output input');
+    list = $('#list-output li');
+    $.each(arr, function (index) {
       if (inputs[index].checked != true) {
-        check.push(value);
+        console.log(list[index]);
+        check.push(list[index]);
       }
     })
     $.fn.view(check, "Unchecked item(s) are:");
@@ -43,14 +53,33 @@ $(document).ready(function () {
   });
   $('#all').on('click', function () {
     $.fn.view(arr, "All item(s) are:");
+    //$('#list-output li').clone().appendTo('#items')
   });
+  $.fn.additem = function (name) {
+    var chk = $('<input />').attr({ type: 'checkbox'});
+    var li = $('<li></li>');
+    arr.push(name);
+    $(li).append(chk).append(name);
+    $('ol[id=list-output]').append(li);
+  }
   $.fn.view = function (array, heading) {
     $('#heading').html(heading);
     if (array.length == 0) {
       $('#items').html('No Item');
     }
     else {
+      console.log("view function:" + array);
       $('#items').html(array.join('<br>'));
     }
   }
+  // $.fn.testing = function(){
+  //   var li = $('#list-output > li > input');
+  //   var li2 = $('#list-output > li');
+  //   console.log(li);
+  //   $.each(arr, function (index, value) {
+  //     if (li[index].checked == true) {
+  //       console.log(li2[index]);
+  //     }
+  //   })
+  // }
 });
